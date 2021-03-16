4c7b283531f58c55feeab1f5a461827c9409c899
hama <21331104th@tama.ac.jp>
2021-03-16 21:03:20 +0900

    CollectionViewCell追加

---
+++ Xib-Test/CollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  Xib-Test
+//
+//  Created by 濱貴大 on 2021/03/14.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+  
+ 
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+}
--- Xib-Test/ViewController.swift
+++ Xib-Test/ViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
 
-    
-    @IBOutlet weak var CollectionView: UICollectionView!
-    
+  
+    let array = ["hoge", "ほげほげ", "Hello!"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        collectionView.dataSource = self
+
+        //xibファイルを読み込む
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        self.collectionView.register(nib, forCellWithReuseIdentifier: "cell")
+
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.array.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+
+        cell.label.text = array[indexPath.row]
+        return cell
+    }
+}
